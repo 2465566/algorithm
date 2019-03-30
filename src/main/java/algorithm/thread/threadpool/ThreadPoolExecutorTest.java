168fc9972018859a7a6fafbffbc616aa82989568
cuijt <cuijt@deepblueai.com>
2019-03-31 02:47:36 +0800

    change Excutors to ThreadPoolExecutor to aviod OOM

--- src/main/java/algorithm/thread/threadpool/ThreadPoolExecutorTest.java
+++ src/main/java/algorithm/thread/threadpool/ThreadPoolExecutorTest.java
@@ -1,14 +1,10 @@
 package algorithm.thread.threadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ThreadPoolExecutorTest {
     public static void main(String[] args) {
-
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = new ThreadPoolExecutor(5, 10, 20, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(5));
         for (int i = 0; i < 10; i++) {
             executorService.submit(new Runnable() {
                 public void run() {
@@ -16,30 +12,5 @@ public class ThreadPoolExecutorTest {
                 }
             });
         }
-
-        ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(3);
-        for (int i = 0; i < 10; i++) {
-            newFixedThreadPool.submit(new Runnable() {
-                public void run() {
-                    System.out.println(System.currentTimeMillis());
-                }
-            });
-        }
-
-        ExecutorService newSingleThreadExecutor = Executors.newSingleThreadExecutor();
-        for (int i = 0; i < 10; i++) {
-            newSingleThreadExecutor.submit(new Runnable() {
-                public void run() {
-                    System.out.println(System.currentTimeMillis());
-                }
-            });
-        }
-
-        ScheduledExecutorService newScheduledThreadPool = Executors.newScheduledThreadPool(3);
-        newScheduledThreadPool.scheduleAtFixedRate(new Runnable() {
-            public void run() {
-                System.out.println(System.currentTimeMillis());
-            }
-        }, 1, 3, TimeUnit.SECONDS);  // 参数： 1 秒后每隔三秒执行一次。
     }
 }
